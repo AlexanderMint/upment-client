abbcf14374ccbc441349fd1e7cdbfc1a3bdce2de
AlexanderMint <webinfo.alexander@gmail.com>
2017-09-06 19:19:03 +0300

    Restruct routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Store from './store'
 import Network from './network'
-import RootRoutes from './routes/root'
+import Routes from './routes'
 
 // Root
 const div = document.createElement('div')
@@ -11,7 +11,7 @@ document.body.append(div)
 ReactDOM.render(
   <Network>
     <Store>
-      <RootRoutes />
+      <Routes />
     </Store>
   </Network>
   , div)
