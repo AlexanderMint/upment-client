5ebfaca27d9c12f7e7db5ddd80d29ab8427f4b51
metronom <metronom72@rambler.ru>
2017-10-10 12:12:02 +0300

    add redux devtools extensions

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import ReduxThunk from 'redux-thunk'
 import ApolloClient from 'network/apollo-client'
 import reducers from './reducers'
 
 export default class Store {
   constructor(history, initialState = {}) {
+    const compositor = process.env.NODE_ENV === 'production' ? compose : composeWithDevTools
     this.data = createStore(
       reducers,
       initialState,
-      compose(
+      compositor(
         applyMiddleware(
           routerMiddleware(history),
           ApolloClient.middleware(),
           ReduxThunk.withExtraArgument(ApolloClient)
         ),
         // eslint-disable-next-line no-underscore-dangle
-        (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+        // (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
       )
     )
   }
