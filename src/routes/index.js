819c5e26ef7b7940eb777a404960baeed8e112ea
AlexanderMint <webinfo.alexander@gmail.com>
2017-10-11 22:48:11 +0300

    Fix react-router-redux

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 import PropTypes from 'prop-types'
 
 import LayoutApp from 'layouts/App'
@@ -8,12 +8,12 @@ import Auth from './auth'
 import Public from './public'
 
 const Routes = ({ history }) => (
-  <Router history={history}>
+  <ConnectedRouter history={history}>
     <LayoutApp>
       <Auth />
       <Public />
     </LayoutApp>
-  </Router>
+  </ConnectedRouter>
 )
 
 Routes.propTypes = {
