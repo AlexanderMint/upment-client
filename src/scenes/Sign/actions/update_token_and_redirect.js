b765ad1837ec12b61e872ec3f1c446e6b917850c
AlexanderMint <webinfo.alexander@gmail.com>
2017-10-24 13:01:32 +0300

    Remove action

--- src/scenes/Sign/actions/update_token_and_redirect.js
+++
@@ -1,8 +0,0 @@
-import { push } from 'react-router-redux'
-import { updateToken } from 'store/actions/token_actions'
-
-export const updateTokenAndRedirect = params => (dispatch) => {
-  dispatch(updateToken(params))
-  dispatch(push('/'))
-}
-
